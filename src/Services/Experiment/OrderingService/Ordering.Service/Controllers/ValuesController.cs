6fae8391b65fc33e5302f5f62ed488df4f9a234f
蒋勇 <jiangy@highzap.com>
2018-10-05 15:24:58 +0800

    调整项目文件结构 新增Ordering测试服务

---
+++ src/Services/Experiment/OrderingService/Ordering.Service/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "ordering.service1", "ordering.service2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
